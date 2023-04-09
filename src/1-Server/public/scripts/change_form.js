e19d197d7e091269c5d127871f0b6eef59a1466c
Pancho157 <hoxocraft65@gmail.com>
2023-04-09 13:51:40 -0300

    Register / Login forms styles working

---
+++ src/1-Server/public/scripts/change_form.js
@@ -0,0 +1,25 @@
+function changeToLoginForm() {
+  document.getElementsByClassName("forms__container")[0].style.height = "400px";
+  document.getElementsByClassName("login_form")[0].style.left = "0";
+  document.getElementsByClassName("register_form")[0].style.left = "150%";
+
+  const form_selector_buttons = document.getElementsByClassName(
+    "form__selector_button"
+  );
+  form_selector_buttons[0].className =
+    "form__selector_button form__selector_button--active";
+  form_selector_buttons[1].className = "form__selector_button";
+}
+
+function changeToRegisterForm() {
+  document.getElementsByClassName("forms__container")[0].style.height = "490px";
+  document.getElementsByClassName("login_form")[0].style.left = "-150%";
+  document.getElementsByClassName("register_form")[0].style.left = "0%";
+
+  const form_selector_buttons = document.getElementsByClassName(
+    "form__selector_button"
+  );
+  form_selector_buttons[0].className = "form__selector_button";
+  form_selector_buttons[1].className =
+    "form__selector_button form__selector_button--active";
+}
