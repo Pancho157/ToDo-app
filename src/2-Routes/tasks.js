91c472feff73c57e9315fb93303c6ed0ffe23ca8
Pancho157 <hoxocraft65@gmail.com>
2023-04-09 15:42:59 -0300

    Sidebar styles made

--- src/2-Routes/tasks.js
+++ src/2-Routes/tasks.js
@@ -2,6 +2,8 @@ const { Router } = require("express");
 
 const tasks = Router();
 
-tasks.get("/:user", () => {});
+tasks.get("/:user/byStatus", (req, res) => {
+  res.render("tasks_by_status");
+});
 
 module.exports = { tasks };
