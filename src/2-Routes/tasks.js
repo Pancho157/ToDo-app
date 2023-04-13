3feb40e560329690e88c78d716f066ed4d987840
Pancho157 <hoxocraft65@gmail.com>
2023-04-13 16:26:40 -0300

    Changed tasks nav - Made calendar view - Modifies logout button

--- src/2-Routes/tasks.js
+++ src/2-Routes/tasks.js
@@ -6,4 +6,16 @@ tasks.get("/byStatus", (req, res) => {
   res.render("tasks_by_status", { layout: "tasks" });
 });
 
+tasks.get("/byPriority", (req, res) => {
+  res.render("tasks_by_priority", { layout: "tasks" });
+});
+
+tasks.get("/byList", (req, res) => {
+  res.render("tasks_list", { layout: "tasks" });
+});
+
+tasks.get("/timeline", (req, res) => {
+  res.render("tasks_by_timeline", { layout: "tasks" });
+});
+
 module.exports = { tasks };
